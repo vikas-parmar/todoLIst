cfc7062542bd0b32de69b1e0a7ba04184d60a0e9
Vikas Panwar <vkparmar003@gmail.com>
2023-02-17 17:05:55 +0530

    Tasks as Data

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const DATA = [
+  { id: "todo-0", name: "Jogging", completed: true },
+  { id: "todo-1", name: "Cricket", completed: false },
+  { id: "todo-2", name: "Sleep", completed: false }
+]
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App tasks ={DATA} />
   </React.StrictMode>,
 )
